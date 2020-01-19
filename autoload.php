3654ae67ceeca33b4d2ffbd3db8d45efd346d112
Richard Cissee <ric@richard-cissee.de>
2020-01-19 16:46:20 +0100

    clean up autoloading of replaced classes

--- autoload.php
+++ autoload.php
@@ -1,7 +1,6 @@
 <?php
 
 use Composer\Autoload\ClassLoader;
-use Fisharebest\Webtrees\Webtrees;
 
 $loader = new ClassLoader();
 $loader->addPsr4('CompactThemes\\', __DIR__);
@@ -11,20 +10,14 @@ $loader->addPsr4('Vesta\\ControlPanel\\', __DIR__ . "/controlpanel");
 $loader->addPsr4('Vesta\\ControlPanel\\Model\\', __DIR__ . "/controlpanel/model");
 $loader->addPsr4('Vesta\\ControlPanel\\Model\\', __DIR__ . "/controlpanel/model/elements");
 
-$loader->register();
-
-//TODO this is a hack (doesn't work if module is renamed)
-//we need a more generic solution for replacing webtrees core classes!
-//
-//if vesta shared places is also used, autoload that first 
-//(Individual extends GedcomRecord)
-foreach (glob(Webtrees::ROOT_DIR . Webtrees::MODULES_PATH . 'vesta_shared_places/autoload.php') as $autoloadFile) {
-  require_once $autoloadFile;
-}
-
+$classMap = array();
 $extend = !class_exists("Fisharebest\Webtrees\Individual", false);
-        
+
 if ($extend) {
   //explicitly load webtrees replacements so that the original files aren't autoloaded
-  require_once __DIR__ . '/replacedWebtrees/Individual.php';
-}  
+  $classMap["Fisharebest\Webtrees\Individual"] = __DIR__ . '/replacedWebtrees/Individual.php';
+}
+
+$loader->addClassMap($classMap);        
+$loader->register(true); //prepend in order to override definitions from default class loader
+
